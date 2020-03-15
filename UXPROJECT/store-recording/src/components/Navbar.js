9850733781fb9e74c1ec3bf063e32ba4e9d28f0c
Kenneth <burningion@hotmail.com>
2020-03-14 20:16:42 -0600

    This is upload 2/6 of the project

--- UXPROJECT/store-recording/src/components/Navbar.js
+++ UXPROJECT/store-recording/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
-import logo from '../logo.svg'
+import logo from '../logo.svg';
+import styled from 'styled-components';
+import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                     {/*/* 
                     https://www.iconfinder.com/icons/1243689/call_phone_icon
                     Creative Commons (Attribution 3.0 Unported);
@@ -15,20 +17,31 @@ export default class Navbar extends Component {
                     className="navbar-brand"/>
                 </Link>
                 <ul className="navbar-nav align-items-center">
-                    <li classname="nav-item ml-5">
+                    <li className="nav-item ml-5">
                         <Link to = "/" className ="nav-link">
                             Products
                         </Link>
                     </li>
                 </ul>
                 <Link to = '/cart' className = "ml-auto">
-                    <button>
+                    <ButtonContainer>
+                    <span className="mr-2">
                         <i className="fas fa-cart-plus"/>
                         My Cart
-                    </button>
+                    </span>
+                    </ButtonContainer>
                 </Link>
-                </nav>
+                </NavWrapper>
             </div>
         )
     }
 }
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+}
+
+`;
