58dc07d033869429ed03418866d7e8fa0aa7c9f5
Kenneth <burningion@hotmail.com>
2020-03-15 12:25:30 -0600

    Upload 3/6 of the first part of the project.

--- UXPROJECT/store-recording/src/context.js
+++ UXPROJECT/store-recording/src/context.js
@@ -8,6 +8,20 @@ class ProductProvider extends Component {
     state = {
         products: storeProducts,
         detailProduct:detailProduct
+    };
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts = () =>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem = {...item};
+            tempProducts = [...tempProducts,singleItem];
+        })
+        this.setState(()=>{
+            return {products: tempProducts}
+        })
+
     }
     handleDetail = () =>{
         console.log('hello from detail');
