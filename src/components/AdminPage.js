e80af2dbedd7a483f6e5edda29b3fc08e01139d2
ConejoMoncada <andresm1996@hotmail.com>
2020-03-28 15:27:30 -0600

    Ultima version

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import firebase from 'firebase'
+
+import GoogleButton from 'react-google-button'
+// or
+
+
 //import * as firebase from 'firebase';
 //import {mainFirebase} from '../config/firebase';
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 export default class AdminPage extends Component {
 
@@ -13,7 +22,11 @@ export default class AdminPage extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
 
+
+
+
     }
+
     saveMessage() {
         // Add a new message entry to the Firebase database.
         return firebase.firestore().collection('messages').add({
@@ -44,23 +57,39 @@ export default class AdminPage extends Component {
             [name]: value
         });
     }
+    signIn() {
+        alert('TODO: Implement Google Sign-In');
+        // Sign into Firebase using popup auth & Google as the identity provider.
+        var provider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(provider);
+      }
+      
+      // Signs-out of Friendly Chat.
+      signOut() {
+        // Sign out of Firebase.
+        firebase.auth().signOut();
+      }
 
     handleSubmit(event) {
         // Saves a new message on the Cloud Firestore.
         this.saveMessage();
         alert('SE HA INGRESADO EXITOSAMENTE EL PRODUCTO A LA BASE DE DATOS');
         event.preventDefault();
+
+
         this.setState({
-            id: 0, 
-            title: '', 
-            img: '', 
-            price: 0, 
-            company: '', 
+            id: 0,
+            title: '',
+            img: '',
+            price: 0,
+            company: '',
             info: ''
         });
     }
 
+
     render() {
+
         return (
 
             <div className="d-flex justify-content-center align-items-center">
@@ -74,7 +103,7 @@ export default class AdminPage extends Component {
 
                             <label>
                                 <h2>
-                                    ITEM ID:     
+                                    ITEM ID:
                         <input
                                         name="id"
                                         type="number"
@@ -154,13 +183,26 @@ export default class AdminPage extends Component {
                                 </h2>
                             </label>
                             <br />
-                            <input type="submit" value="Submit" />
+                            <input type="submit" value="Submit to Database" />
+                            <br />
+                            <br />
+                            <GoogleButton
+                                    onClick={() => {this.signIn() }}
+                                />
+                            <div>
+      
+                            </div>
+
+                            
+
                         </div>
+
                     </div>
                 </form>
             </div>
+
+
         );
+
     }
-
-
 }
